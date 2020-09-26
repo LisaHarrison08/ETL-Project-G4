82eb9abfd4febeddc39fe2841fd1aa74ddf13b52
Lisa Harrison <lmharrison0810@gmail.com>
2020-09-26 11:44:46 -0400

    removed groupby and updated queries

--- queries.sql
+++ queries.sql
@@ -1,23 +1,22 @@
 -- Create tables for raw data to be loaded into
 CREATE TABLE google_data (
-States              VARCHAR(80)     PRIMARY KEY     NOT NULL,
-Retail_Recreation   FLOAT       NOT NULL,
-Grocery_Pharmacy    FLOAT       NOT NULL,
-Parks               FLOAT       NOT NULL
+States              VARCHAR(80) NOT NULL,
+Retail_Recreation   FLOAT,
+Grocery_Pharmacy    FLOAT,
+Parks               FLOAT,
+Dates               VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE covid_data (
-States      VARCHAR(80)      PRIMARY KEY    NOT NULL,
+Dates       VARCHAR(80) NOT NULL,
+States      VARCHAR(80) NOT NULL,
 fips        INT     NOT NULL,
 cases       INT     NOT NULL,
 deaths      INT     NOT NULL
 );
 
-SELECT g.states, g.grocery_pharmacy, c.cases
+SELECT g.states, g.dates, g.grocery_pharmacy, c.cases
 FROM google_data g
 JOIN covid_data c
-USING (states);
+ON g.states = c.states AND g.dates=c.dates;
 
-SELECT g.states, g.retail_recreation, g.grocery_pharmacy, c.deaths, c.cases 
-FROM google_data g
-JOIN covid_data c ON g.states = c.states;
